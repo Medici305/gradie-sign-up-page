87267da22c7be03b8bbf8f603628d90a8750b3c5
unknown <Medici305>
2021-11-15 17:42:17 +0000

    3. layout for sign up set up.

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -2,8 +2,20 @@ import React from "react";
 
 const SignUpForm = () => {
   return (
-    <div>
-      <h1>Hey</h1>
+    //   1. Container
+    <div className="flex sm:h-screen justify-center items-center bg-gradient-to-r from-pink-400 via-purple-500 to-pink-500 box-sizing">
+      {/* 2. Row */}
+      <div className="flex h-screen sm:h-4/5 flex-col lg:flex-row m-lg-12 w-full sm:w-10/12 shadow-lg bg-white sm:rounded-lg">
+        {/* 3. Columns */}
+        <div className="flex flex-auto justify-center items-center bg-sign-up-5 bg-center bg-cover bg-opacity-50 bg-no-repeat rounded-b-lg sm:rounded-lg">
+          <div className="flex justify-center items-center text-center rounded-lg h-2/3 w-10/12 backdrop-filter backdrop-blur-md">
+            <h1 className="">
+              "Motivational Text"
+            </h1>
+          </div>
+        </div>
+        <div className="flex-auto bg-white sm:rounded-lg"></div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "sign-up": "url('/src/Assets/Background.png')",
+        "sign-up-2": "url('/src/Assets/duotone.png')",
+        "sign-up-3": "url('/src/Assets/sunset.jpg')",
+        "sign-up-4": "url('/src/Assets/pexel-1.jpg')",
+        "sign-up-5": "url('/src/Assets/nipsey.jpg')",
+      },
+    },
     screens: {
       sm: "480px",
       md: "768px",
